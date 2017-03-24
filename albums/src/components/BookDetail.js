8cf3ff6528d47566698bcc6e038524d324461d6a
Michael McDuffee <mrmcduff@stripe.com>
2017-03-24 16:53:48 -0700

    end l44, adding cards with text stacks

--- albums/src/components/BookDetail.js
+++ albums/src/components/BookDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,10 +7,21 @@ const BookDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.book.title}</Text>
+        <View></View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.book.title}</Text>
+          <Text>{props.book.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
 };
 
 export default BookDetail;
