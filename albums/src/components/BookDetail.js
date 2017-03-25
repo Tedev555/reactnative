9a0c6fd4fea8efcaa8db1cde0bbde73fab3e0921
Michael McDuffee <mrmcduff@stripe.com>
2017-03-24 17:09:26 -0700

    adding big pictures of tswift. end l46

--- albums/src/components/BookDetail.js
+++ albums/src/components/BookDetail.js
@@ -4,21 +4,34 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const BookDetail = ({ book }) => {
-  const { title, artist, thumbnail_image } = book;
+  const { title, artist, thumbnail_image, image } = book;
+  const {
+    bodyImageStyle,
+    headerImageStyle,
+    headerContentStyle,
+    headerImageContainerStyle,
+    titleStyle } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={headerImageContainerStyle}>
           <Image
-            style={styles.headerImageStyle}
+            style={headerImageStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={titleStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={bodyImageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
@@ -29,9 +42,25 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  titleStyle: {
+    fontSize: 18
+  },
   headerImageStyle: {
     height: 50,
     width: 50
+  },
+  headerImageContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  bodyImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 
