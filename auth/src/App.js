9fde24c9aa61b679535b67f9333e06928d723f7e
Michael McDuffee <mrmcduff@stripe.com>
2017-03-28 14:30:49 -0700

    complete application for auth. end l76

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import { LoginForm } from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -14,13 +15,42 @@ class App extends Component {
       storageBucket: 'auth-47dad.appspot.com',
       messagingSenderId: '1080732268494'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user);
+        this.setState({ loggedIn: true });
+      } else {
+        console.log(user);
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button
+              onPress={() => firebase.auth().signOut()}
+            >
+              Log out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <CardSection><Spinner size="large" /></CardSection>;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
