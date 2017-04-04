7ab0c5208e7248f1ff876c378a5ae0ea89684303
Michael McDuffee <mrmcduff@stripe.com>
2017-04-03 17:09:45 -0700

    layout animation spring is magic

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  LayoutAnimation,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItemComponent extends Component {
 
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
 
     if (expanded) {
       return (
-        <Text>{library.description}</Text>
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
       );
     }
   }
@@ -45,7 +58,6 @@ const styles = {
 };
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state);
   const expanded =
     state.selectedLibraryId === ownProps.library.id;
   return { expanded };
